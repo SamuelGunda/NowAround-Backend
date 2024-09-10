eb67b263739a75ef0bc2eb5d452f1112b85ca2f9
Samuel Gunda <samuel.gunda@kosickaakademia.sk>
2024-09-10 21:17:55 +0200

    add helthcheck controller, create unit test project, add first simple test

---
+++ NowAround.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace NowAround.Api.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldReturnOk()
+    {
+        Assert.True(true);
+    }
+}
---
+++ NowAround.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NowAround.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Healthy");
+    }
+}
