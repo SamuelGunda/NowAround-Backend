2e3dcd19d29c69bcd616978d91e971042bf22573
Samuel Gunda <samuel.gunda@kosickaakademia.sk>
2024-11-12 10:25:50 +0100

    Base repository start

---
+++ NowAround.Api/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NowAround.Api.Database;
+
+namespace NowAround.Api.Repositories;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<TEntity> GetByIdAsync(int id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(int id);
+}
+
+internal abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+    
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public Task<TEntity> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
